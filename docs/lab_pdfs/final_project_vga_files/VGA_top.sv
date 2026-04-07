5e7a9a4d63727577750c5f16ea8d23782e20986c
Vikas Dhiman <wecacuee@users.noreply.github.com>
2026-04-07 08:45:45 -0400

    fix

--- docs/lab_pdfs/final_project_vga_files/VGA_top.sv
+++ docs/lab_pdfs/final_project_vga_files/VGA_top.sv
@@ -42,8 +42,8 @@ module VGA_top(
     //////////// VGA //////////
     output           [3:0]      VGA_B, //Output Blue
     output           [3:0]      VGA_G, //Output Green
-    output                      VGA_HS,//Horizontal Sync
     output           [3:0]      VGA_R, //Output Red
+    output                      VGA_HS,//Horizontal Sync
     output                      VGA_VS,//Vertical Sync
 
     //////////// Accelerometer //////////
@@ -61,8 +61,8 @@ module VGA_top(
     inout                       ARDUINO_RESET_N
  );
 
-wire			[9:0]		X_pix;			//Location in X of the driver
-wire			[9:0]		Y_pix;			//Location in Y of the driver
+wire			[9:0]		X_pix;			// Location in X of the driver
+wire			[9:0]		Y_pix;			// Location in Y of the driver
 
 wire			[0:0]		H_visible;		//H_blank?
 wire			[0:0]		V_visible;		//V_blank?
@@ -71,12 +71,12 @@ wire		   [0:0]		pixel_clk;		//Pixel clock. Every clock a pixel is being drawn.
 wire			[9:0]		pixel_cnt;		//How many pixels have been output.
 
 reg			[11:0]		pixel_color;	//12 Bits representing color of pixel, 4 bits for R, G, and B
-										//4 bits for Blue are in most significant position, Red in least
+										//4 bits for Red are in most significant position, Blue in least
 
-parameter [11:0] WIDTH  = 12'd640;
-parameter [11:0] HEIGHT = 12'd480;
-parameter [11:0] TWO    = 12'd2;
-parameter [11:0] FIVE   = 12'd5;
+parameter [11:0] WIDTH    = 12'd640;
+parameter [11:0] HEIGHT   = 12'd480;
+parameter [11:0] TWO      = 12'd2;
+parameter [11:0] FIVE     = 12'd5;
 parameter [11:0] SEVEN    = 12'd7;
 parameter [11:0] THIRTEEN = 12'd13;
 
@@ -98,18 +98,18 @@ end
 //Pass pins and current pixel values to display driver
 DE10_VGA VGA_Driver
 (
-	.clk_50(MAX10_CLK1_50),
-	.pixel_color(pixel_color),
-	.VGA_BUS_R(VGA_R), 
-	.VGA_BUS_G(VGA_G), 
-	.VGA_BUS_B(VGA_B), 
-	.VGA_HS(VGA_HS), 
-	.VGA_VS(VGA_VS), 
-	.X_pix(X_pix), 
-	.Y_pix(Y_pix), 
-	.H_visible(H_visible),
-	.V_visible(V_visible), 
-	.pixel_clk(pixel_clk),
+	.clk_50(MAX10_CLK1_50),   // input to the driver
+	.pixel_color(pixel_color), // input to the driver
+	.VGA_BUS_R(VGA_R),         // output
+	.VGA_BUS_G(VGA_G),         // output
+	.VGA_BUS_B(VGA_B),         // output
+	.VGA_HS(VGA_HS),           // output
+	.VGA_VS(VGA_VS),           // output
+	.X_pix(X_pix),             // output what pixel we are drawing right now
+	.Y_pix(Y_pix),             // output what pixel we are drawing right now
+	.H_visible(H_visible),     // H_blank?
+	.V_visible(V_visible),
+	.pixel_clk(pixel_clk),     // Pixel clock. Every clock a pixel is being drawn.
 	.pixel_cnt(pixel_cnt)
 );
 
